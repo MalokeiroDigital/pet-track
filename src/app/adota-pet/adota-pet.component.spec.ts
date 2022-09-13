2ab962ffdd5c794fb5c1e492d9c7240821d9bcb2
MalokeiroDigital <javaentra21@yahoo.com>
2022-09-13 12:03:27 -0300

    first commit

---
+++ src/app/adota-pet/adota-pet.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AdotaPetComponent } from './adota-pet.component';
+
+describe('AdotaPetComponent', () => {
+  let component: AdotaPetComponent;
+  let fixture: ComponentFixture<AdotaPetComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ AdotaPetComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AdotaPetComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
